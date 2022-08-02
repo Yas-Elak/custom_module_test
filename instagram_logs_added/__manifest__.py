5ad036c5eccbed140ec943b370ae01e8b96ccea3
Yas-Elak <elalaoui87@gmail.com>
2022-08-02 08:09:47 +0200

    Add files via upload

---
+++ instagram_logs_added/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "instagram_logs_added",
+
+    'summary': """
+        Add logs for instagram""",
+
+    'description': """
+        Add logs for instagram
+    """,
+
+    'author': "",
+    'website': "",
+    'license': 'LGPL-3',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'social', 'social_instagram'],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
+}
+
